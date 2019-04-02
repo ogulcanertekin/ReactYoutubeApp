5c32f6baed339f06301ca3b3f3e99dcba042b40a
ogulcanertekin <ogulcanertekinn@gmail.com>
2019-04-02 17:57:29 +0300

    Displaying Video -Using  Iframe and videoId

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -4,8 +4,14 @@ const VideoDetail = ({video})=>{        //destructing props.video --> video
     if(!video){
         return <div>Loading...</div>;
     }
+
+    const VideoSrc = `https://www.youtube.com/embed/${video.id.videoId}`
+
     return(
         <div>
+            <div className="ui embed">
+                <iframe src={VideoSrc}/>
+            </div>
             <div className="ui segment">
                 <h4 className="ui header">{video.snippet.title}</h4>
                 <p>{video.snippet.description}</p>
