b8b749edbd254e62162f82e45f3267afb8164b10
ogulcanertekin <ogulcanertekinn@gmail.com>
2019-04-02 16:37:03 +0300

    List of Videos with Images

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,9 +1,19 @@
 import React from 'react';
-
+import VideoItem from './VideoItem';
+/*
 const VideoList = (props) => {
     //props.videos
     return <div>{props.videos.length}</div>
 
 }
+*/
+const VideoList = ({videos}) => {           //destructing ES6
 
+    const renderedList = videos.map((video)=>{          // map fonks kullanarak her bir video için  VideoItem componenti kullanılıyor.
+        return <VideoItem video={video}/>;
+    });
+
+    return <div>{renderedList}</div>
+
+}
 export default VideoList;
